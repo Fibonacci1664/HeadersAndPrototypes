e7ece1b32a072aaf2afa1b84baabf5132bfde344
Fibonacci1664 <dave.green1664@yahoo.co.uk>
2019-06-11 12:45:07 +0100

    First commit

---
+++ src/HeadersAndPrototypes.cpp
@@ -0,0 +1,29 @@
+/*
+ * HeadersAndPrototypes.cpp
+ *
+ *  Created on: 11 Jun 2019
+ *      Author: Dave
+ */
+
+
+#include<iostream>
+#include "utils.h"
+
+using namespace std;
+
+
+
+int main()
+{
+
+	doSomething();
+	doSomething();
+
+	return 0;
+}
+
+void doSomething()		// This is a function Definition, something that includes a codeblock.
+{
+	cout << "Hello" << endl;
+}
+
---
+++ src/utils.h
@@ -0,0 +1,16 @@
+/*
+ * utils.h
+ *
+ *  Created on: 11 Jun 2019
+ *      Author: Dave
+ */
+
+#ifndef UTILS_H_
+#define UTILS_H_
+
+void doSomething();	// This is a Prototype otherwise known as a Declaration.
+
+
+
+
+#endif /* UTILS_H_ */
